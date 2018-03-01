13f0ba0289dcef08ab844a360b49a862b562c9d7
joy-chen <joychendao@icloud.com>
2018-03-01 11:50:39 +0800

    Initial commit

---
+++ app/src/main/java/com/joy/customview/viewgroup/CustomViewGroup.java
@@ -0,0 +1,132 @@
+package com.joy.customview.viewgroup;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by joy on 3/1/18.
+ */
+
+public class CustomViewGroup extends ViewGroup {
+    
+    public CustomViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public CustomViewGroup(Context context) {
+        super(context);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    /**
+     * 计算所有ChildView的宽度和高度 然后根据ChildView的计算结果，设置自己的宽和高
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        /**
+         * 获得此ViewGroup上级容器为其推荐的宽和高，以及计算模式
+         */
+        int withMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int withSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int width = 0;
+        int height = 0;
+
+        int cCount = getChildCount();
+
+        int cWidth = 0;
+        int cHeight = 0;
+
+        MarginLayoutParams cParams = null;
+
+        int lHeight =0;
+        int rHeight = 0;
+
+        int tWidth = 0;
+        int bWith =0;
+
+        View childView;
+        for (int i = 0; i < cCount; i++) {
+            childView = getChildAt(i);
+            cWidth = childView.getMeasuredWidth();
+            cHeight = childView.getMeasuredHeight();
+            cParams = (MarginLayoutParams) childView.getLayoutParams();
+
+            if (i == 0 || i == 1) {
+                tWidth += cWidth + cParams.leftMargin + cParams.rightMargin;
+            }
+            if (i == 2 || i == 3) {
+                bWith += cWidth + cParams.leftMargin + cParams.rightMargin;
+            }
+            if (i == 0 || i == 2) {
+                lHeight += cHeight + cParams.topMargin + cParams.bottomMargin;
+            }
+            if (i == 1 || i == 3) {
+                rHeight += cHeight + cParams.topMargin + cParams.bottomMargin;
+            }
+        }
+
+        width = Math.max(tWidth, bWith);
+        height = Math.max(lHeight, rHeight);
+
+        setMeasuredDimension((withMode == MeasureSpec.EXACTLY) ? withSize : width,
+                (heightMode == MeasureSpec.EXACTLY) ? heightSize : height);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int cCount = getChildCount();
+        int cWidth = 0;
+        int cHeight = 0;
+        MarginLayoutParams cParams = null;
+
+        View childView;
+        for (int i = 0; i < cCount; i++) {
+            childView = getChildAt(i);
+            cWidth = childView.getMeasuredWidth();
+            cHeight = childView.getMeasuredHeight();
+            cParams = (MarginLayoutParams) childView.getLayoutParams();
+            int cl = 0, ct = 0, cr = 0, cb = 0;
+
+            switch (i) {
+                case 0:
+                    cl = cParams.leftMargin;
+                    ct = cParams.topMargin;
+                    break;
+                case 1:
+                    cl = getWidth() - cWidth - cParams.leftMargin - cParams.rightMargin;
+                    ct = cParams.topMargin;
+                    break;
+                case 2:
+                    cl = cParams.leftMargin;
+                    ct = getHeight() - cHeight - cParams.bottomMargin;
+                    break;
+                case 3:
+                    cl = getWidth() - cWidth - cParams.leftMargin - cParams.rightMargin;
+                    ct = getHeight() - cHeight - cParams.bottomMargin;
+                    break;
+            }
+            cr = cl + cWidth;
+            cb = cHeight + ct;
+            childView.layout(cl, ct, cr, cb);
+        }
+    }
+}
